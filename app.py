ab27f8828f4b3c83b4649aea92018677af7d3805
Lucky1234V2 <s.chaoui@hetic.net>
2023-11-22 14:35:23 +0100

    update 2 app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 import os
 from datetime import datetime
 from io import BytesIO
@@ -9,14 +10,13 @@ from PIL import Image
 
 app = Flask(__name__)
 
-# Assurez-vous que ce dossier existe
 PDF_FOLDER = os.path.join('static', 'pdfs')
 os.makedirs(PDF_FOLDER, exist_ok=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
-    pdf_filepath = None  # Initialise la variable pour qu'elle soit accessible en dehors du if
+    pdf_filepath = None
     if request.method == 'POST':
         if 'image' not in request.files:
             return 'Aucun fichier sélectionné', 400
@@ -27,35 +27,29 @@ def upload_image():
         if file:
             image = Image.open(file.stream)
 
-            # Utilisez BytesIO au lieu de sauvegarder l'image sur le disque
             img_io = BytesIO()
-            image.save(img_io, 'JPEG')
+            image.save(img_io, format='JPEG')
             img_io.seek(0)
+            img_base64 = base64.b64encode(img_io.getvalue()).decode()
 
             pdf = FPDF()
             pdf.add_page()
-            # Utilisez BytesIO ici
-            pdf.image(img_io, x=0, y=0, w=210)
+            pdf.image('@' + img_base64, x=0, y=0, w=210)
 
-            # Créer un nom de fichier unique pour le PDF
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
             pdf_filename = f"image_to_pdf_{timestamp}.pdf"
             pdf_filepath = os.path.join(PDF_FOLDER, pdf_filename)
             pdf.output(pdf_filepath)
 
-            # Génère le chemin relatif pour l'utilisation dans le template
             pdf_rel_path = os.path.join('pdfs', pdf_filename)
 
-            # Redirection vers le téléchargement
             return redirect(url_for('download_pdf', pdf_filename=pdf_filename))
 
-    # Si pas POST ou après l'enregistrement de l'image, affiche la page normalement
     return render_template('index.html', pdf_filepath=pdf_rel_path if pdf_filepath else None)
 
 
 @app.route('/download/<pdf_filename>')
 def download_pdf(pdf_filename):
-    # Sécurise le nom du fichier pour éviter les chemins d'accès relatifs
     safe_pdf_filename = os.path.basename(pdf_filename)
     return send_from_directory(PDF_FOLDER, safe_pdf_filename, as_attachment=True)
 

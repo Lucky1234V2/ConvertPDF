c72d325672e6e09a8addef579a7d4593b9afd33f
Lucky1234V2 <s.chaoui@hetic.net>
2023-11-22 15:05:40 +0100

    ud

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from datetime import datetime
 
 from flask import (Flask, redirect, render_template, request,
@@ -15,7 +16,7 @@ os.makedirs(PDF_FOLDER, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
-    pdf_filepath = None  # Initialise la variable pour qu'elle soit accessible en dehors du if
+    pdf_filepath = None
     if request.method == 'POST':
         if 'image' not in request.files:
             return 'Aucun fichier sélectionné', 400
@@ -24,29 +25,29 @@ def upload_image():
             return 'Aucun fichier sélectionné', 400
 
         if file:
-            image_path = os.path.join(PDF_FOLDER, "temp_image.jpg")
-            image = Image.open(file.stream)
-            image.save(image_path)
+            # Utilisation d'un répertoire temporaire pour stocker l'image
+            with tempfile.TemporaryDirectory() as temp_dir:
+                image_path = os.path.join(temp_dir, "temp_image.jpg")
+                image = Image.open(file.stream)
+                image.save(image_path)
 
-            pdf = FPDF()
-            pdf.add_page()
-            pdf.image(image_path, x=0, y=0, w=210)
+                pdf = FPDF()
+                pdf.add_page()
+                pdf.image(image_path, x=0, y=0, w=210)
 
-            # Créer un nom de fichier unique pour le PDF
-            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-            pdf_filename = f"image_to_pdf_{timestamp}.pdf"
-            pdf_filepath = os.path.join(PDF_FOLDER, pdf_filename)
-            pdf.output(pdf_filepath)
+                timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+                pdf_filename = f"image_to_pdf_{timestamp}.pdf"
+                pdf_filepath = os.path.join(PDF_FOLDER, pdf_filename)
+                pdf.output(pdf_filepath)
 
-            os.remove(image_path)  # Supprime l'image temporaire
+                # Génère le chemin relatif pour le template
+                pdf_rel_path = os.path.join('pdfs', pdf_filename)
 
-            # Génère le chemin relatif pour l'utilisation dans le template
-            pdf_rel_path = os.path.join('pdfs', pdf_filename)
+        # Supprime l'image temporaire après avoir créé le PDF
+        os.remove(image_path)
 
-            # Redirection vers le téléchargement
-            return redirect(url_for('download_pdf', pdf_filename=pdf_filename))
+        return redirect(url_for('download_pdf', pdf_filename=pdf_filename))
 
-    # Si pas POST ou après l'enregistrement de l'image, affiche la page normalement
     return render_template('index.html', pdf_filepath=pdf_rel_path if pdf_filepath else None)
 
 
